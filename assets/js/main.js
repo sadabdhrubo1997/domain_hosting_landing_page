af42e7beaf3e3d35f9e597b56d57893a8cdffbb4
sadabdhrubo1997 <53494264+sadabdhrubo1997@users.noreply.github.com>
2020-12-25 23:00:22 +0600

    large screen responsive problem solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,52 @@ $("#hambarger").click(function(){
 
 
 
+  // Responsive html font size
+
+//   onload problem solving
+  let iw = window.innerWidth;
+  let rootScreenWidth = 1600;
+  let rootFontPercentage = 53.35;     
+ 
+  if (iw <= 1366) {
+      $('#my_html').attr("style", `font-size:${rootFontPercentage}%`);
+      
+  }else{
+      let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+      $('#my_html').attr("style", `font-size:${currentFontSize}%`);    
+  }
+      
+
+
+//   or resizing problem solving
+
+$(window).resize(function(){
+    let iw = window.innerWidth;
+    let rootScreenWidth = 1600;
+    let rootFontPercentage = 53.35;     
+   
+    if (iw <= 1366) {
+        $('#my_html').attr("style", `font-size:${rootFontPercentage}%`);
+        
+    }else{
+        let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+        $('#my_html').attr("style", `font-size:${currentFontSize}%`);    
+    }
+})
+
+
+  // Responsive html font size end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
